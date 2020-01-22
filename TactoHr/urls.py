75a1626686797d880f5812c505e9e6e292add9ce
Muzammil Ansari <muzammil.ansari@tacto.in>
2020-01-22 20:13:45 +0530

    feed app

--- TactoHr/urls.py
+++ TactoHr/urls.py
@@ -18,5 +18,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('contact/',include('contact.urls'))
+    path('feed/',include('feed.urls'))
 ]
--- feed/urls.py
+++ feed/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
-from contact import views
+from feed import views
 
 urlpatterns = [
    path('index',views.index,name="index")
