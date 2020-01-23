201e089bfb29aebb6fce92ce0377a939b45c0c1d
indresh <indresh.mahor@tacto.in>
2020-01-23 11:14:26 +0530

    test add

--- feed/views.py
+++ feed/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(req):
-    return HttpResponse("<h1>Hi there again</h1>")
+    return HttpResponse("<h1>Hi there again from chandresh</h1>")
